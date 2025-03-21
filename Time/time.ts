3c6643ca47eb472f520090839a3eca0988625edb
Vinicius Valencio Talhiaferro <vinicius.valencio@voeazul.com.br>
2025-03-21 11:31:38 -0300

    versao 2

--- Time/time.ts
+++ Time/time.ts
@@ -8,7 +8,7 @@ interface Time {
     nomeAbreviado: string;
   }
   
-  function atualizarTabelaTime() {
+  function atualizarTabelaTimes() {
     tabelaTime.innerHTML = "";
     times.forEach((t : Time)  =>{
     tabelaTime.innerHTML += `
@@ -16,10 +16,60 @@ interface Time {
            <td>${t.nomeTime}</td>
            <td>${t.nomeAbreviado}</td>
            <td>
-                <button onclick="editarCampeonato(${t.id})"> Editar </button>
-                <button onclick="removerCampeonato(${t.id})"> Remover </button>
+                <button onclick="editarTime(${t.id})"> Editar </button>
+                <button onclick="removerTime(${t.id})"> Remover </button>
            </td>
       </tr>
     `;
     })
   }
+
+  function editarTime(id:number) {
+    const time = times.find((t : Time) => t.id == id);
+    if(!time) return;
+  
+    (document.getElementById("nomeTime") as HTMLInputElement).value = time.nomeTime;
+    (document.getElementById("nomeAbreviado") as HTMLInputElement).value = time.nomeAbreviado;
+  
+    const timeIndex = times.findIndex((t:Time) => t.id == id);
+      //valida se encontrou alum item
+      if(timeIndex !== -1) {
+        //remove da lista
+        times.splice(timeIndex, 1);
+      }
+      salvarLocalStorageTimes()
+      atualizarTabelaTimes()
+  }
+
+  function removerTime (id:number) {
+    const timeIndex = times.findIndex((t:Time) => t.id == id);
+      //valida se encontrou alum item
+      if(timeIndex !== -1) {
+        //remove da lista
+        times.splice(timeIndex, 1);
+      }
+      salvarLocalStorageTimes()
+      atualizarTabelaTimes()
+  }
+
+  function salvarLocalStorageTimes() {
+    let timesSalvar = JSON.stringify(times);
+    localStorage.setItem("times", timesSalvar);
+  }
+
+  function salvarTimes(event:Event) {
+    event?.preventDefault(); //cancelar o disparo do evento
+    const novoTime: Time = {
+        id: Date.now(),
+        nomeTime: (document.getElementById("timeAbreviado") as HTMLInputElement).value,
+        nomeAbreviado: (document.getElementById("nomeAbreviado") as HTMLInputElement).value,
+    };
+    times.push(novoTime)
+    atualizarTabelaTimes()
+    salvarLocalStorageTimes()
+    formTime.reset()
+    alert('Cadastrado com sucesso')
+  }
+
+  formTime.addEventListener("submit", salvarTimes)
+  atualizarTabelaTimes()
